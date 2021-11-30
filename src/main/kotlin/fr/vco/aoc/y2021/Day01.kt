828822a8bb15c89e3e8f7af523ee0529aeeb9f3c
Vincent <vincentcoubray@gmail.com>
2021-11-30 22:57:30 +0100

    Day 00 : Init project

---
+++ src/main/kotlin/fr/vco/aoc/y2021/Day01.kt
@@ -0,0 +1,6 @@
+package fr.vco.aoc.y2021
+
+fun main() {
+   val input = readIntput("Day01")
+   input.forEach(::println)
+}
---
+++ src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
@@ -0,0 +1,6 @@
+package fr.vco.aoc.y2021
+
+import java.io.File
+
+fun readIntput(fileName : String) = File ("src/main/resources","$fileName.txt").readLines()
+
