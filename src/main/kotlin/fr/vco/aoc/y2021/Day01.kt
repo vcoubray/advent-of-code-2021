679af7e0a1da1b3153575a7edf88c00d91b9cc9f
vcoubray <vcoubray@sedona.fr>
2021-12-01 10:22:56 +0100

    Day01 - part 1 & 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day01.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day01.kt
@@ -1,6 +1,17 @@
 package fr.vco.aoc.y2021
 
 fun main() {
-   val input = readIntput("Day01")
-   input.forEach(::println)
+    val input = readLines("Day01").map { it.toInt() }
+
+    println("Part 1 : ${getIncreased(input, 1)}")
+    println("Part 2 : ${getIncreased(input, 3)}")
+}
+
+fun getIncreased(depths: List<Int>, range: Int): Int {
+    val depthRanges = List(depths.size - range + 1) { depths.subList(it, it + range).sum() }
+    var increaseCount = 0
+    for (i in 1 until depthRanges.size) {
+        if (depthRanges[i] > depthRanges[i - 1]) increaseCount++
+    }
+    return increaseCount
 }
--- src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
@@ -2,5 +2,5 @@ package fr.vco.aoc.y2021
 
 import java.io.File
 
-fun readIntput(fileName : String) = File ("src/main/resources","$fileName.txt").readLines()
+fun readLines(fileName : String) = File ("src/main/resources","$fileName.txt").readLines()
 
