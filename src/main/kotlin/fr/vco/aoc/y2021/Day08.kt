3cb3ca65da08d6f38beeffda38cb4577b6bd9f49
Vincent <vincentcoubray@gmail.com>
2021-12-08 20:39:37 +0100

    Day 08 : Simplify code

--- src/main/kotlin/fr/vco/aoc/y2021/Day08.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day08.kt
@@ -1,70 +1,62 @@
 package fr.vco.aoc.y2021
 
+typealias SegmentDigit = Set<Char>
+
+private fun emptySegmentDigit() = emptySet<Char>()
+
 fun main() {
     val input = readLines("Day08")
 
-    val numbers = input.map { line ->
-        val (inputNumbers, outputNumbers) = line.split(" | ").map { numbers ->
-            numbers.split(" ").map { it.toSortedString() }
-        }
+    val displays = input.map { line ->
+        val (inputNumbers, outputNumbers) = line.split(" | ").map { it.toSegmentDigitList() }
         DigitDisplay(inputNumbers, outputNumbers)
     }
 
-    println("Part 1 : ${numbers.sumOf { it.countNumbersWithUniqueSize() }}")
-    println("Part 2 : ${numbers.sumOf { it.outputToInt() }}")
+    println("Part 1 : ${displays.sumOf { it.countNumbersWithUniqueSize() }}")
+    println("Part 2 : ${displays.sumOf { it.outputToInt() }}")
 }
 
-private fun String.toSortedString() = this.toSortedSet().joinToString("")
+private fun String.toSegmentDigitList() = this.split(" ").map { it.toSet() }
+private operator fun SegmentDigit.contains(digit: SegmentDigit) = digit.all { it in this }
 
 private class DigitDisplay(
-    val input: List<String>,
-    val output: List<String>
+    val input: List<SegmentDigit>,
+    val output: List<SegmentDigit>
 ) {
-    val numbersMap = MutableList(10) { "" }
-    val reverseMap: Map<String, String>
+    val digitsMap = MutableList(10) { emptySegmentDigit() }
+    val reverseMap: Map<SegmentDigit, String>
 
     private companion object {
         val UNIQUE_SIZES = listOf(2, 3, 4, 7)
     }
 
     init {
-        findNumbersWithUniqueSize()
-        findNumbersWith5Segments()
-        findNumbersWith6Segments()
-        reverseMap = numbersMap.mapIndexed { i, it -> it to i.toString() }.toMap()
-    }
+        val digitsBySize = input.groupBy { it.size }
 
-    fun countNumbersWithUniqueSize() = output.count { it.length in UNIQUE_SIZES }
+        // Digit with unique Size (1, 4, 7 and 8)
+        digitsMap[1] = digitsBySize[2]!!.first()
+        digitsMap[4] = digitsBySize[4]!!.first()
+        digitsMap[7] = digitsBySize[3]!!.first()
+        digitsMap[8] = digitsBySize[7]!!.first()
 
-    fun findNumbersWithUniqueSize() {
-        numbersMap[1] = input.first { it.length == 2 }
-        numbersMap[4] = input.first { it.length == 4 }
-        numbersMap[7] = input.first { it.length == 3 }
-        numbersMap[8] = input.first { it.length == 7 }
-    }
-
-    fun findNumbersWith6Segments() {
-        val (n0Andn9, n6) = input.filter { it.length == 6 }.partition { numbersMap[1].all { segment -> segment in it } }
-        numbersMap[6] = n6.first()
-
-        val (n9, n0) = n0Andn9.partition { numbersMap[4].all { segment -> segment in it } }
-        numbersMap[0] = n0.first()
-        numbersMap[9] = n9.first()
-    }
-
-    fun findNumbersWith5Segments() {
-        val (n3, n2and5) = input.filter { it.length == 5 }.partition { numbersMap[1].all { segment -> segment in it } }
-        numbersMap[3] = n3.first()
-
-        val topLeftSegment = numbersMap[4] - numbersMap[3]
+        // Digit with Size 5 (2, 3 and 5)
+        val (n3, n2and5) = digitsBySize[5]!!.partition { digitsMap[1] in it }
+        digitsMap[3] = n3.first()
+        val topLeftSegment = digitsMap[4] - digitsMap[3]
         val (n5, n2) = n2and5.partition { topLeftSegment in it }
-        numbersMap[2] = n2.first()
-        numbersMap[5] = n5.first()
+        digitsMap[2] = n2.first()
+        digitsMap[5] = n5.first()
+
+        // Digit with Size 6 (0, 6 and 9)
+        val (n0Andn9, n6) = digitsBySize[6]!!.partition { digitsMap[1] in it }
+        digitsMap[6] = n6.first()
+        val (n9, n0) = n0Andn9.partition { digitsMap[4] in it }
+        digitsMap[0] = n0.first()
+        digitsMap[9] = n9.first()
+
+        reverseMap = digitsMap.mapIndexed { i, it -> it to i.toString() }.toMap()
     }
 
+    fun countNumbersWithUniqueSize() = output.count { it.size in UNIQUE_SIZES }
     fun outputToInt() = output.joinToString("") { reverseMap[it]!! }.toInt()
-}
-
-private operator fun String.minus(string: String): String {
-    return (this.toList() - string.toSet()).joinToString("")
 }
