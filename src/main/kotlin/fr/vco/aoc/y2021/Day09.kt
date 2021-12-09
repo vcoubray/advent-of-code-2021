60f2fecac4839fc2809ecb719ca96be729b0d246
vcoubray <vcoubray@sedona.fr>
2021-12-09 14:06:08 +0100

    Day 09 : Refacto Part 1 to prepare part 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day09.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day09.kt
@@ -3,25 +3,28 @@ package fr.vco.aoc.y2021
 fun main() {
     val input = readLines("Day09")
 
-    val floor = input.map { line -> line.map { it.digitToInt() } }
-    println(floor)
-    var res = 0
-    floor.forEachIndexed { y, line ->
-        line.forEachIndexed { x, c ->
-            println("$x $y -> $c ")
-            println(floor.getNeighbors(x, y))
-
-            if (c < floor.getNeighbors(x, y).minOrNull()!!) {
-                res += c + 1
-            }
-        }
-    }
-    println("Part 1: $res")
+    val floor = input.toCellList()
+    println("Part 1: ${floor.fold(0) { acc, a -> acc + if(a.isMin(floor)) a.risk else 0 }}")
 }
 
-fun List<List<Int>>.getNeighbors(x: Int, y: Int) = listOfNotNull(
-    this.getOrNull(y - 1)?.getOrNull(x),
-    this.getOrNull(y + 1)?.getOrNull(x),
-    this.getOrNull(y)?.getOrNull(x - 1),
-    this.getOrNull(y)?.getOrNull(x + 1)
+
+private fun List<String>.get(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x)
+private fun List<String>.convertCoord(x: Int, y: Int) = this.get(x, y)?.let { this.first().length * y + x }
+private fun List<String>.getNeighbors(x: Int, y: Int) = listOfNotNull(
+    this.convertCoord(x - 1, y),
+    this.convertCoord(x + 1, y),
+    this.convertCoord(x, y - 1),
+    this.convertCoord(x, y + 1),
 )
+
+private fun List<String>.toCellList() = this.flatMapIndexed { y, line ->
+    line.mapIndexed { x, c ->
+        Cell(c.digitToInt(), this.getNeighbors(x, y))
+    }
+}
+
+private data class Cell(val height: Int, val neighborsId: List<Int>) {
+    val risk = height + 1
+
+    fun isMin(floor: List<Cell>) = this.height < this.neighborsId.minOf { floor[it].height }
+}
