923a1a5e356b0de3aaf00838acc95d56e1477a85
vcoubray <vcoubray@sedona.fr>
2021-12-09 14:31:19 +0100

    Day 09 : Part 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day09.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day09.kt
@@ -1,12 +1,14 @@
 package fr.vco.aoc.y2021
 
+import java.util.*
+
 fun main() {
     val input = readLines("Day09")
 
     val floor = input.toCellList()
-    println("Part 1: ${floor.fold(0) { acc, a -> acc + if(a.isMin(floor)) a.risk else 0 }}")
+    println("Part 1: ${floor.fold(0) { acc, a -> acc + if (a.isMin(floor)) a.risk else 0 }}")
+    println("Part 2: ${floor.getPoolsSize().sortedDescending().take(3).reduce { acc, a -> acc * a }}")
 }
-
 
 private fun List<String>.get(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x)
 private fun List<String>.convertCoord(x: Int, y: Int) = this.get(x, y)?.let { this.first().length * y + x }
@@ -27,4 +29,34 @@ private data class Cell(val height: Int, val neighborsId: List<Int>) {
     val risk = height + 1
 
     fun isMin(floor: List<Cell>) = this.height < this.neighborsId.minOf { floor[it].height }
+}
+
+private fun List<Cell>.getPool(id: Int): Set<Int> {
+    if (this[id].height == 9) return emptySet()
+    val visited = mutableSetOf<Int>()
+    val toVisit = LinkedList<Int>()
+    toVisit.add(id)
+    while (!toVisit.isEmpty()) {
+        val current = toVisit.pop()
+        visited.add(current)
+        this[current].neighborsId.forEach {
+            if (this[it].height != 9 && !visited.contains(it))
+                toVisit.addLast(it)
+        }
+    }
+    return visited
+}
+
+private fun List<Cell>.getPoolsSize(): List<Int> {
+    val pools = MutableList(this.size) { -1 }
+    val poolSize = mutableListOf<Int>()
+    this.forEachIndexed { i, cell ->
+        if (pools[i] < 0) {
+            if (cell.height == 9) pools[i] = 0
+            val pool = this.getPool(i)
+            poolSize.add(pool.size)
+            pool.forEach { pools[it] = poolSize.size - 1 }
+        }
+    }
+    return poolSize
 }
