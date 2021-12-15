3ce205d767394df6127311f48a9d2bda163342a0
Vincent <vincentcoubray@gmail.com>
2021-12-15 20:02:52 +0100

    Day 15 : Part 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day15.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day15.kt
@@ -3,7 +3,7 @@ package fr.vco.aoc.y2021
 import java.util.*
 
 fun main() {
-    val input = readLines("Day15")
+    val input = readLines("Day15").map { it.map { c -> c.digitToInt() } }
 
     val cavern = input.toTileList()
     println("Part 1 : ${cavern.dijkstra()}")
@@ -13,38 +13,37 @@ fun main() {
 
 }
 
-private fun List<String>.get(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x)
-private fun List<String>.convertCoord(x: Int, y: Int) = this.get(x, y)?.let { this.first().length * y + x }
-private fun List<String>.getNeighbors(x: Int, y: Int) = listOfNotNull(
+private fun List<List<Int>>.get(x: Int, y: Int) = this.getOrNull(y)?.getOrNull(x)
+private fun List<List<Int>>.convertCoord(x: Int, y: Int) = this.get(x, y)?.let { this.first().size * y + x }
+private fun List<List<Int>>.getNeighbors(x: Int, y: Int) = listOfNotNull(
     this.convertCoord(x - 1, y),
     this.convertCoord(x + 1, y),
     this.convertCoord(x, y - 1),
     this.convertCoord(x, y + 1),
 )
 
-private fun List<String>.multiply(times: Int) = List(this.size * times) { this[it % this.size].increaseRisk(it / this.size).multiply(times) }
-private fun String.multiply(times: Int) = List(times) { this.increaseRisk(it) }.joinToString("")
-private fun String.increaseRisk(increase: Int) = this.map { it.digitToInt() }.map { ((it - 1 + increase) % 9) + 1 }.joinToString("")
+private fun List<List<Int>>.multiply(times: Int) = List(this.size * times) { this[it % this.size].increaseRisk(it / this.size).multiplyLine(times) }
+private fun List<Int>.multiplyLine(times: Int) = List(times) { this.increaseRisk(it) }.flatten()
+private fun List<Int>.increaseRisk(increase: Int) = this.map { ((it - 1 + increase) % 9) + 1 }
 
 private data class Tile(val risk: Int, val neighborsId: List<Int>)
 
-private fun List<String>.toTileList() = this.flatMapIndexed { y, line ->
-    line.mapIndexed { x, c ->
-        Tile(c.digitToInt(), this.getNeighbors(x, y))
-    }
+private fun List<List<Int>>.toTileList() = this.flatMapIndexed { y, line ->
+    line.mapIndexed { x, c -> Tile(c, this.getNeighbors(x, y)) }
 }
 
 private fun List<Tile>.dijkstra(start: Int = 0, end: Int = this.size - 1): Int {
-    val toVisit = LinkedList<Pair<Int, Int>>().apply { addFirst(0 to 0) }
+    val costComparator = compareBy <Pair<Int, Int>>{ it.second }
+    val toVisit = PriorityQueue(costComparator).apply { add(0 to 0) }
     val visited = mutableMapOf<Int, Int>()
 
     while (!toVisit.isEmpty()) {
-        val (tileId, cost) = toVisit.pop()
+        val (tileId, cost) = toVisit.poll()
         val current = this[tileId]
         if (visited.getOrDefault(tileId, Int.MAX_VALUE) > cost + current.risk) {
             visited[tileId] = cost + current.risk
             current.neighborsId.forEach {
-                toVisit.addLast(it to cost + current.risk)
+                toVisit.add(it to cost + current.risk)
             }
         }
     }
