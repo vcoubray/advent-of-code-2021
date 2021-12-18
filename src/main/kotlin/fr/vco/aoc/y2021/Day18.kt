1ab8fbf74f5f64677f3df82f549852ea3046729c
Vincent <vincentcoubray@gmail.com>
2021-12-18 19:33:11 +0100

    Day 18 : Init Part 1

---
+++ src/main/kotlin/fr/vco/aoc/y2021/Day18.kt
@@ -0,0 +1,44 @@
+package fr.vco.aoc.y2021
+
+fun main() {
+    val input = readLines("Day18")
+
+    val numbers = input.map{it.toSnailFishNumber()}
+    numbers.forEach(::println)
+}
+
+fun String.toSnailFishNumber() : SnailFishNumber {
+    if (!this.startsWith('[') ) return SnailFishNumber.Number(this.toInt())
+    val s = this.drop(1).dropLast(1)
+    val sepId = s.findSeparatorIndex()
+    return SnailFishNumber.Pair(s.take(sepId).toSnailFishNumber(), s.drop(sepId+1).toSnailFishNumber())
+}
+
+fun String.findSeparatorIndex() :Int {
+    var counter = 0
+    this.forEachIndexed { i, it ->
+        when {
+            it == '[' -> counter++
+            it == ']' -> counter--
+            it == ',' && counter == 0 -> return i
+        }
+    }
+    return -1
+}
+
+abstract class SnailFishNumber (val parent : SnailFishNumber? = null) {
+
+    operator fun plus(number: SnailFishNumber) = Pair(this, number)
+    operator fun plus(number: Int) = Pair(this, Number(number))
+    abstract fun magnetude() : Int
+
+    class Number(val value: Int) : SnailFishNumber() {
+        override fun magnetude() = value
+        override fun toString() = "$value"
+    }
+
+    class Pair(val left: SnailFishNumber, val right: SnailFishNumber) : SnailFishNumber() {
+        override fun magnetude()= 3 * left.magnetude()+ 2 * right.magnetude()
+        override fun toString() = "[$left,$right]"
+    }
+}
