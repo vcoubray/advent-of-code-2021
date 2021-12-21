09daf8ab15e2e28b65378197a2ec1fd2a1488cf9
Vincent <vincentcoubray@gmail.com>
2021-12-22 00:28:59 +0100

    Day 19 : Read input

--- src/main/kotlin/fr/vco/aoc/y2021/Day19.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day19.kt
@@ -1,21 +1,33 @@
 package fr.vco.aoc.y2021
 
 fun main() {
+    val input = readLines("Day19")
 
-
+    val scanners = input.split {it == "" }.map { scanner ->
+        println("aaa" + scanner.first())
+        val (id) = """(\d+)""".toRegex().find(scanner.first())!!.destructured
+        Scanner(id.toInt(), scanner.drop(1).map { line ->
+            line.split(",").map { it.toInt() }.let { (x, y, z) -> Point3D(x, y, z) }
+        })
+    }
+    println(scanners)
 }
 
 
 data class Point3D(val x: Int = 0, val y: Int = 0, val z: Int = 0) {
     operator fun plus(p: Point3D) = Point3D(x + p.x, y + p.y, z + p.z)
     operator fun minus(p: Point3D) = Point3D(x - p.x, y - p.y, z - p.z)
-    operator fun unaryMinus() = Point3D(-x,-y,-z)
-
+    operator fun unaryMinus() = Point3D(-x, -y, -z)
+    fun rotateX() = copy(y = z, z = -y)
+    fun rotateY() = copy(x = -z, z = x)
+    fun rotateZ() = copy(x = y, y = -x)
 }
 
 data class Transformation(val translation: Point3D = Point3D(), val rotate: Point3D = Point3D()) {
-    fun translate(p : Point3D) = p + translation
-    fun rotate(p: Point3D) = Point3D(
+    fun translate(p: Point3D) = p + translation
+}
 
-    )
+data class Scanner(val id: Int, val beacon: List<Point3D>, val transformation: Transformation = Transformation()) {
+    override fun equals(other: Any?) = if (other is Scanner) id == other.id else false
+    override fun hashCode() = id.hashCode()
 }
--- src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Utils.kt
@@ -5,3 +5,12 @@ import java.io.File
 private fun String.getInputFile() = File("src/main/resources", "$this.txt")
 fun readLines(fileName: String) = fileName.getInputFile().readLines()
 fun readFirstLine(fileName: String) = fileName.getInputFile().readLines().first()
+
+fun List<String>.split(predicate: (String) -> Boolean): List<List<String>> {
+    val list = mutableListOf(mutableListOf<String>())
+    this.forEach {
+        if (predicate(it)) list.add(mutableListOf())
+        else list.last().add(it)
+    }
+    return list
+}
