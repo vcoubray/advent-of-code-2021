908af0a38f623ed65ed7402c238a152a82532d10
Vincent <vincentcoubray@gmail.com>
2021-12-22 23:48:24 +0100

    Day 19 : Part 1 & 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day19.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day19.kt
@@ -1,5 +1,10 @@
 package fr.vco.aoc.y2021
 
+import java.util.*
+import kotlin.math.absoluteValue
+
+const val OVERLAP_BEACON_COUNT = 12
+
 fun main() {
     val input = readLines("Day19")
 
@@ -13,12 +18,9 @@ fun main() {
         )
     }
 
-    val scanner = scanners.first()
-    val transform = Transformation(Point3D(800, -95, 567))
-    val scanner2 = scanner.transform(transform)
-
-
-    scanner.isOverlap(scanner2)
+    val map = scanners.toSynchronizedScanners()
+    println("Part 1 : ${map.getBeacons().size}")
+    println("Part 2 : ${map.maxDistance()}")
 }
 
 
@@ -26,38 +28,74 @@ data class Point3D(val x: Int = 0, val y: Int = 0, val z: Int = 0) {
     operator fun plus(p: Point3D) = Point3D(x + p.x, y + p.y, z + p.z)
     operator fun minus(p: Point3D) = Point3D(x - p.x, y - p.y, z - p.z)
     operator fun unaryMinus() = Point3D(-x, -y, -z)
-    fun rotateX() = copy(y = z, z = -y)
-    fun rotateY() = copy(x = -z, z = x)
-    fun rotateZ() = copy(x = y, y = -x)
+    private fun rotateX() = copy(y = z, z = -y)
+    private fun rotateY() = copy(x = -z, z = x)
+    private fun rotateZ() = copy(x = y, y = -x)
 
     fun rotateX(times: Int) = (0 until times).fold(this) { acc, _ -> acc.rotateX() }
     fun rotateY(times: Int) = (0 until times).fold(this) { acc, _ -> acc.rotateY() }
     fun rotateZ(times: Int) = (0 until times).fold(this) { acc, _ -> acc.rotateZ() }
+
+    fun dist(point: Point3D) = (this - point).dist()
+    fun dist() = x.absoluteValue + y.absoluteValue + z.absoluteValue
 }
 
-data class Transformation(val translation: Point3D = Point3D(), val rotate: Point3D = Point3D()) {
-    fun translate(p: Point3D) = p + translation
-    fun rotate(p: Point3D) = p.rotateX(rotate.x).rotateY(rotate.y).rotateZ(rotate.z)
-    fun transform(p: Point3D) = rotate(p).apply(::println) + translation
+data class Transformation(val translation: Point3D = Point3D(), val rotation: Point3D = Point3D()) {
+    fun rotate(p: Point3D) = p.rotateX(rotation.x).rotateY(rotation.y).rotateZ(rotation.z)
+    fun transform(p: Point3D) = rotate(p) + translation
 
 }
 
-data class Scanner(val id: Int, val beacons: Set<Point3D>, val transformation: Transformation = Transformation()) {
+data class Scanner(val id: Int, val beacons: Set<Point3D>, val position: Point3D = Point3D()) {
     override fun equals(other: Any?) = if (other is Scanner) id == other.id else false
     override fun hashCode() = id.hashCode()
-    fun transform(transformation: Transformation) =
-        Scanner(id, beacons.map(transformation::transform).toSet(), transformation)
+    private fun transform(transformation: Transformation) =
+        Scanner(id, beacons.map(transformation::transform).toSet(), transformation.transform(position))
 
-    fun isOverlap(scanner: Scanner): Boolean {
+    private fun isOverlap(scanner: Scanner): Scanner? {
         this.beacons.forEach { beaconRef ->
             scanner.beacons.forEach {
-                val translate = beaconRef - it
-                println(translate)
-                println(scanner.transform(Transformation(translate)).beacons.intersect(this.beacons).size)
+                val translatedScanner = scanner.transform(Transformation(beaconRef - it))
+                if (translatedScanner.beacons.intersect(this.beacons).size >= OVERLAP_BEACON_COUNT)
+                    return translatedScanner
             }
         }
-        return false
+        return null
     }
 
+    fun transformScannerIfOverlap(scanner: Scanner): Scanner? {
+        repeat(4) { rotateX ->
+            repeat(4) { rotateY ->
+                repeat(4) { rotateZ ->
+                    val transformation = Transformation(rotation = Point3D(rotateX, rotateY, rotateZ))
+                    val s = scanner.transform(transformation)
+                    this.isOverlap(s)?.let { return it }
+                }
+            }
+        }
+        return null
+    }
+}
 
+fun List<Scanner>.toSynchronizedScanners(): List<Scanner> {
+    val visited = LinkedList<Scanner>().also { it.add(this.first()) }
+    val toVisit = this.drop(1).toMutableList()
+    var visitedIndex = 0
+    while (toVisit.isNotEmpty()) {
+        val refScanner = visited[visitedIndex]
+        toVisit.forEach { refScanner.transformScannerIfOverlap(it)?.let(visited::add) }
+        toVisit.removeIf { it in visited }
+        visitedIndex++
+    }
+    return visited
+}
+
+fun List<Scanner>.getBeacons() = this.fold(mutableSetOf<Point3D>()) { acc, a -> acc.also { it.addAll(a.beacons) } }
+
+fun List<Scanner>.maxDistance(): Int {
+    var dist = 0
+    this.forEachIndexed { i, scanner ->
+        dist = this.drop(i).maxOf { scanner.position.dist(it.position) }.takeIf { it > dist } ?: dist
+    }
+    return dist
 }
