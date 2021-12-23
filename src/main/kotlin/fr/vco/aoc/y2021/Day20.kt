1865fd3076fc737105aaf8eaa525bf3be76c9265
vcoubray <vcoubray@sedona.fr>
2021-12-23 18:07:59 +0100

    Day 20 : Part 1 & 2

---
+++ src/main/kotlin/fr/vco/aoc/y2021/Day20.kt
@@ -0,0 +1,38 @@
+package fr.vco.aoc.y2021
+
+fun main() {
+    val input = readLines("Day20")
+
+    val enhancementAlgo = input.first().map { it == '#' }
+    val image = input.drop(1).filterNot { it == "" }.map { line -> line.map { it == '#' } }
+
+    println("Part 1 : ${image.enhanceMutli(enhancementAlgo,2).countLit()}")
+    println("Part 2 : ${image.enhanceMutli(enhancementAlgo,50).countLit()}")
+}
+
+fun List<List<Boolean>>.countLit() = this.sumOf { line -> line.count { it } }
+
+fun List<List<Boolean>>.enhanceMutli(algo: List<Boolean>, times: Int): List<List<Boolean>> {
+    return (0 until times).fold(this) { acc, turn -> acc.enhance(algo, turn) }
+}
+
+fun List<List<Boolean>>.enhance(algo: List<Boolean>, turn: Int): List<List<Boolean>> {
+   return List(this.size + 2) { y ->
+        List(this.first().size + 2) { x ->
+            algo[getEnhancementValue(x - 1, y - 1, if (algo.first()) turn % 2 != 0 else false)]
+        }
+    }
+}
+
+fun List<List<Boolean>>.getEnhancementValue(x: Int, y: Int, defaultValue: Boolean): Int {
+    return buildList {
+        for (iy in -1..1) {
+            for (ix in -1..1) {
+                this.add(this@getEnhancementValue.getValue(x + ix, y + iy, defaultValue))
+            }
+        }
+    }.joinToString("") { if (it) "1" else "0" }.toInt(2)
+}
+
+fun List<List<Boolean>>.getValue(x: Int, y: Int, defaultValue: Boolean) =
+    this.getOrNull(y)?.getOrNull(x) ?: defaultValue
