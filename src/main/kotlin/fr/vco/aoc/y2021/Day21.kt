961a3cfda173865acb3fde9609b3f507e16fa95d
vcoubray <vcoubray@sedona.fr>
2021-12-29 16:19:14 +0100

    Day 21 : Part 2

--- src/main/kotlin/fr/vco/aoc/y2021/Day21.kt
+++ src/main/kotlin/fr/vco/aoc/y2021/Day21.kt
@@ -8,34 +8,69 @@ fun main() {
         Player(pos.toInt())
     }
 
-    println("Part 1 : ${players.game()}")
+    println("Part 1 : ${players.playDeterministicGame()}")
+    println("Part 2 : ${players.playQuantumGame().maxOrNull()}")
 }
 
 data class Player(var pos: Int, var score: Int = 0) {
-    fun play(dice: DeterministicDice) {
-        val rolls = dice.roll() + dice.roll() + dice.roll()
-        pos = ((pos + rolls - 1) % 10) + 1
+    fun play(roll: Int) {
+        pos = ((pos + roll - 1) % 10) + 1
         score += pos
     }
 }
 
-class DeterministicDice {
+class DeterministicDie {
     var value: Int = 0
     var rolls: Int = 0
-    fun roll(): Int {
+    private fun roll(): Int {
         value = if (value == 100) 1 else (value + 1)
         rolls++
         return value
     }
+
+    fun roll(times: Int): Int = (0 until times).sumOf { roll() }
 }
 
-private fun List<Player>.game(): Int {
+private fun List<Player>.playDeterministicGame(
+    scoreMax: Int = 1000,
+    die: DeterministicDie = DeterministicDie()
+): Int {
+    val players = this.map { it.copy() }
     var currentPlayer = 0
-    val dice = DeterministicDice()
 
-    while (this.none { it.score >= 1000 }) {
-        this[currentPlayer].play(dice)
+    while (players.none { it.score >= scoreMax }) {
+        players[currentPlayer].play(die.roll(3))
         currentPlayer = 1 - currentPlayer
     }
-    return this.minOf { it.score } * dice.rolls
+    return players.minOf { it.score } * die.rolls
+}
+
+private fun <T> List<T>.combination(list: List<T>, operation: (T, T) -> T) =
+    this.map { a -> list.map { b -> operation(a, b) } }.flatten()
+
+private class QuantumDie(faces: Int = 3, rollsCount: Int = 3) {
+    val rolls = List(rollsCount) { List(faces) { it + 1 } }
+        .reduce { acc, rolls -> acc.combination(rolls) { a, b -> a + b } }
+        .groupingBy { it }.eachCount()
+}
+
+private fun List<Player>.playQuantumGame(
+    maxScore: Int = 21,
+    die: QuantumDie = QuantumDie(),
+    currentPlayer: Int = 0,
+): List<Long> {
+    val wins = mutableListOf(0L, 0L)
+    die.rolls.map { (roll, occurrence) ->
+        this.map { it.copy() }.also {
+            it[currentPlayer].play(roll)
+            if (it[currentPlayer].score >= maxScore) {
+                wins[currentPlayer] += occurrence.toLong()
+            } else {
+                it.playQuantumGame(maxScore, die, 1 - currentPlayer).forEachIndexed { i, win ->
+                    wins[i] += win * occurrence
+                }
+            }
+        }
+    }
+    return wins
 }
